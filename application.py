cf322a2e709c6702ab2d4dc6401d2fa7cc0cfb6d
Abhishek Kulkarni <miabhishekkulkarni@gmail.com>
2019-03-13 23:04:15 -0400

    fix main function and make output console readable

--- application.py
+++ application.py
@@ -1,126 +1,82 @@
-from constants import PLAYERS, TEAMS
+from constants import PLAYERS
 import sys
 
 
-
-def menu():
+def main(team):
 	print("""BASKETBALL TEAM STATS TOOL
 
----- MENU----
+	---- MENU----
+	Here are your choices:
+	1) Display Team Stats
+	2) Quit
 
- Here are your choices:
-  1) Display Team Stats
-  2) Quit
+	""")
 
-""")
-
-
-response = int(input("Enter an option >  "))
-
-
-def main():
+	response = int(input("Enter an option >  "))
 	if response == 1:
-		team = input("""
-		1) Panthers
-		2) Bandits
-		3) Warriors
-					""")
+		team = int(input("""
+1) Panthers
+2) Bandits
+3) Warriors
+					"""))
 		if team == 1:
-			print_panthers()
-		elif team == 2:
-			print_bandits()
-		elif team == 3:
-			print_warriors()
-	print("Press ENTER to continue")
-	if response == 2:
-		sys.exit()
+			print_players(my_panthers)
 
+		elif team == 2:
+			print_players(my_bandits)
+
+		elif team == 3:
+			print_players(my_warriors)
+
+	elif response == 2:
+		sys.exit()
+	return team
 
 
 names = []
+experienced_dict = []
+unexperienced_dict = []
+
 
 for i in PLAYERS:
 	names.append(i['name'])
 
-panthers = names[:6]
-bandits = names[6:12]
-warriors = names[12:18]
+for i in PLAYERS:
+	if i['experience'] == "YES":
+		experienced_dict.append(i)
+
+for i in PLAYERS:
+	if i['experience'] == "NO":
+		unexperienced_dict.append(i)
 
 
-def print_panthers():
-	for panther in panthers:
-		print(panther, ',', end="")
+experienced_players = []
+unexperienced_players = []
 
 
-def print_bandits():
-	for bandit in bandits:
-		print(bandit, ",", end="")
+for i in experienced_dict:
+	experienced_players.append(i['name'])
 
 
-def print_warriors():
-	for warrior in warriors:
-		print(warrior, ",", end="")
+for i in unexperienced_dict:
+	unexperienced_players.append(i['name'])
 
 
-if __name__ == "__main__":
-	menu()
+my_panthers = experienced_players[:3] + unexperienced_players[:3]
+my_bandits = experienced_players[3:6] + unexperienced_players[3:6]
+my_warriors = experienced_players[6:9] + unexperienced_players[6:9]
 
-#
-# names = []
-# experienced = []
-# unexperienced = []
-#
-#
-# for i in PLAYERS:
-#     names.append(i['name'])
-#
-# for i in PLAYERS:
-#     if i['experience'] == "YES":
-#         experienced.append(i)
-#
-# for i in PLAYERS:
-#     if i['experience'] == "NO":
-#         unexperienced.append(i)
-#
-# panthers = names[:6]
-# bandits = names[6:12]
-# warriors = names[12:18]
-#
-#
-# def print_panthers():
-#     for panther in panthers:
-#         print(panther, ',', end="")
-#
-#
-# def print_bandits():
-#     for bandit in bandits:
-#         print(bandit, ",", end="")
-#
-#
-# def print_warriors():
-#     for warrior in warriors:
-#         print(warrior, ",", end="")
-#
-#
-#
-# newex = []
-# unex = []
-#
-#
-# for i in experienced:
-#     newex.append(i['name'])
-#
-#
-# for i in unexperienced:
-#     unex.append(i['name'])
-#
-# print(newex)
-# print(unex)
-# my_panthers = newex[:3] + unex[:3]
-# print(my_panthers)
-# my_bandits = newex[3:6] + unex[3:6]
-# print(my_bandits)
-# my_warriors = newex[6:9] + unex[6:9]
-# print(my_warriors)
 
-print("Did it commit?")
+def print_players(team):
+	print("""
+	Team: {}
+	--------------------
+	Total players: 6""")
+
+	print("Players on Team:\n")
+	for player in team:
+		print(player, end=", ")
+
+
+if __name__ == '__main__':
+	main(team=None)
